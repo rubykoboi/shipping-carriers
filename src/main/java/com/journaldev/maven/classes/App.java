e5dd8faa02d374997ef64df01facbc298cee4df6
rubykoboi <dianne.robbi@safavieh.com>
2022-08-26 13:14:05 -0400

    set TURKON BL regex and now applying pdf splitting process after analyzing steps

--- src/main/java/com/journaldev/maven/classes/App.java
+++ src/main/java/com/journaldev/maven/classes/App.java
@@ -10,6 +10,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,6 +29,24 @@ public class App {
 	private final static String ULTIMATE_FILE_PATH = "C:\\SC";
 	private List<String> filesList;
 	
+	private final static String CMA_BL_REGEX = "([^a-zA-Z0-9\\s*][A-Z]{3}[0-9]{7})[^a-zA-Z0-9\\s*]";
+	private final static String EVERGREEN_BL_REGEX = "([^a-zA-Z0-9|\\s*][A-Z]{3}[0-9]{7})[\\s*|^a-zA-Z0-9]";
+	private final static String MAERSK_BL_REGEX = "";
+	private final static String MSC_BL_REGEX = "";
+	private final static String TURKON_BL_REGEX = "\\s*(\\d{8}\\d{0,2})(?=BILL OF LADING)";
+
+	public static Pattern PATTERN_CMA;
+	public static Pattern PATTERN_EVERGREEN;
+	public static Pattern PATTERN_MAERSK;
+	public static Pattern PATTERN_MSC;
+	public static Pattern PATTERN_TURKON;
+	
+	public static Matcher matcher;
+	
+	public static File currentTextFile;
+	public static int currentStartPage;
+	public static int currentEndPage;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -42,6 +62,13 @@ public class App {
 		filesList = retrieveAllFiles();
 		int fileTypes[] = new int[filesList.size()];
 
+		// INITIALIZE ALL BL# PATTERNS
+		PATTERN_CMA = Pattern.compile(CMA_BL_REGEX);
+		PATTERN_EVERGREEN = Pattern.compile(EVERGREEN_BL_REGEX);
+		PATTERN_MAERSK = Pattern.compile(MAERSK_BL_REGEX);
+		PATTERN_MSC = Pattern.compile(MSC_BL_REGEX);
+		PATTERN_TURKON = Pattern.compile(TURKON_BL_REGEX);
+		
 		for(int a=0 ; a<filesList.size(); a++) {
 			File file = new File(filesList.get(a));
 			out("read in file ["+a+"] : "+filesList.get(a));
@@ -52,19 +79,30 @@ public class App {
 			// TO-DO: PERFORM BL# SEARCH ON EACH FILE BY TYPE
 			switch (fileTypes[a]) {
 				case CMA_TYPE:
+					out("processing CMA type");
 					processCMA();
+					break;
 				case EVERGREEN_TYPE:
+					out("processing EVERGREEN type");
 					processEVERGREEN();
+					break;
 				case MAERSK_TYPE:
+					out("processing MAERSK type");
 					processMAERSK();
+					break;
 				case MSC_TYPE:
+					out("processing MSC type");
 					processMSC();
+					break;
 				case TURKON_TYPE:
+					out("processing TURKON type");
 					processTURKON();
+					break;
 				default:
 					break;
 			}
 		}
+		
 		/**
 		 * Step 1: Read in one pdf file
 		 * 		2: Determine the type of file
@@ -76,28 +114,112 @@ public class App {
 	
 	public static void processCMA() {
 		// TO-DO: process CMA files
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(currentTextFile));
+			String currentLine = br.readLine();
+			out("before CMA while");
+			while(currentLine != null) {
+				matcher = PATTERN_CMA.matcher(currentLine);
+				if(matcher.find()) {
+					out("found CMA match: " + matcher.group(1));
+				}
+				currentLine = br.readLine();
+			}
+			br.close();
+		} catch (Exception e) {
+			out("We got an exception from processCMA " + e);
+			e.printStackTrace();
+		}
 	}
 
 	public static void processEVERGREEN() {
-		// TO-DO: process EVERGREEN files
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(currentTextFile));
+			String currentLine = br.readLine();
+			out("before EVERGREEN while");
+			while(currentLine != null) {
+				matcher = PATTERN_EVERGREEN.matcher(currentLine);
+				if(matcher.find()) {
+					out("found EVERGREEN match: " + matcher.group(1));
+				}
+				currentLine = br.readLine();
+			}
+			br.close();
+		} catch (Exception e) {
+			out("We got an exception from processEVERGREEN " + e);
+			e.printStackTrace();
+		}
 	}
 	
 	public static void processMAERSK() {
-		// TO-DO: process MAERSK files
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(currentTextFile));
+			String currentLine = br.readLine();
+			out("before MAERSK while");
+			while(currentLine != null) {
+				matcher = PATTERN_MAERSK.matcher(currentLine);
+				if(matcher.find()) {
+					out("found MAERSK match: " + matcher.group(1));
+				}
+				currentLine = br.readLine();
+			}
+			br.close();
+		} catch (Exception e) {
+			out("We got an exception from processMAERSK " + e);
+			e.printStackTrace();
+		}
 	}
 	
 	public static void processMSC() {
-		// TO-DO: process MSC files
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(currentTextFile));
+			String currentLine = br.readLine();
+			out("before MSC while");
+			while(currentLine != null) {
+				matcher = PATTERN_MSC.matcher(currentLine);
+				if(matcher.find()) {
+					out("found MSC match: " + matcher.group(1));
+				}
+				currentLine = br.readLine();
+			}
+			br.close();
+		} catch (Exception e) {
+			out("We got an exception from processMSC " + e);
+			e.printStackTrace();
+		}
 	}
 	
 	public static void processTURKON() {
-		// TO-DO: process TURKON files
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(currentTextFile));
+			String currentLine = br.readLine();
+			int blCounter = 0;
+			out("before TURKON while");
+			while(currentLine != null) {
+				matcher = PATTERN_TURKON.matcher(currentLine);
+				if(matcher.find()) {
+					blCounter++;
+					out("the following line seems to have matched the matcher: ");
+					out(currentLine);
+					out(blCounter + " <================found TURKON match: " + matcher.group(1));
+					// TO-DO: split pdf
+					
+				}
+				currentLine = br.readLine();
+			}
+			out("out of while");
+			br.close();
+		} catch (Exception e) {
+			out("We got an exception from processTURKON " + e);
+			e.printStackTrace();
+		}
 	}
 	
 	public static int determineFileType(File file) {
 		out("inside determineFileType method");
 		try {
 			PDDocument doc = PDDocument.load(file);
+			currentTextFile = file;
 			PDFTextStripper pdfStripper = new PDFTextStripper();
 			String text = pdfStripper.getText(doc);
 			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\SC\\text.txt"));
@@ -109,7 +231,6 @@ public class App {
 			
 			File textfile = new File("C:\\SC\\text.txt");
 			BufferedReader br = new BufferedReader(new FileReader(textfile));
-			
 			String currentLine = br.readLine();
 			
 			for (int line = 0; currentLine != null; line ++) {
